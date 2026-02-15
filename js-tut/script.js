01eb2250668152265282ed6483604eda6ef35914
mohammadpmf <mohammad.pfallah@gmail.com>
2026-02-15 20:31:17 +0330

    finish file 11 (destructuring)

--- js-tut/script.js
+++ js-tut/script.js
@@ -25,22 +25,56 @@
 // const foundedNumber = numbers.find(x => x > 10)
 // console.log(foundedNumber)
 // __________________reduce____________________
-const numbers = [1, 2, 3, 4, 5]
-const result = numbers.reduce(
-    (accumulator, currentItem) => accumulator + currentItem,
-    0
-);
-console.log(result);
-const result2 = numbers.reduce(
-    (accumulator, currentItem) => accumulator * currentItem,
-    1
-);
-console.log(result2);
-const result3 = numbers.reduce(
-    (accumulator, currentItem) => accumulator + currentItem + 2,
-    5
-);// اینو اضافه برای خودم نوشتم.
-// اکومولیتور خروجی مرحله قبل هست تو که تو مرحله بعد اجرا میشه. اما دفعه اول
-// ما مرحله قبل نداریم. پس بهش مقدار اولیه میدیم. آخرین بار هم خروجی رو به عنوان
-// نتیجه ردیوس میکنه و به ما تحویل میده.
-console.log(result3);
+// const numbers = [1, 2, 3, 4, 5]
+// const result = numbers.reduce(
+//     (accumulator, currentItem) => accumulator + currentItem,
+//     0
+// );
+// console.log(result);
+// const result2 = numbers.reduce(
+//     (accumulator, currentItem) => accumulator * currentItem,
+//     1
+// );
+// console.log(result2);
+// const result3 = numbers.reduce(
+//     (accumulator, currentItem) => accumulator + currentItem + 2,
+//     5
+// );// اینو اضافه برای خودم نوشتم.
+// // اکومولیتور خروجی مرحله قبل هست تو که تو مرحله بعد اجرا میشه. اما دفعه اول
+// // ما مرحله قبل نداریم. پس بهش مقدار اولیه میدیم. آخرین بار هم خروجی رو به عنوان
+// // نتیجه ردیوس میکنه و به ما تحویل میده.
+// console.log(result3);
+
+// __________________template literals____________________
+// const name = "Mohammad"
+// const age = 35
+// const text = `My name is ${name} and my age is ${age} years old.`
+// console.log(text)
+// __________________ Destructuring ____________________
+const data = {
+    name: "Mohammad",
+    age: 35,
+    address: { city: "Rasht", street: "Azadegan" },
+}
+// const name = data.name
+// const age = data["age"]
+// const city = data.address.city
+// ...
+// yek raveshe sade tar (Destructuring)
+// const { name, address, age } = data
+// const { city, street } = address
+// in 2 marhale i bood. dakhelesh ham meshe dobare destructre kard ba : . yani in shekli:
+// const { name address: { street, city }, age } = data
+// montaha age khode address ro lazem darim, oonam joda minvisim yani in shekli:
+const {
+    name,
+    address,
+    address: { street, city },
+    age
+} = data
+// تو ریکت خیلی استفاده میشه.
+console.log(name)
+console.log(age)
+console.log(address)
+console.log(city)
+console.log(street)
