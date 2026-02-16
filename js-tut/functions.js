725ae64cee7327ceeb7b9b7c41207741e2f885e5
mohammadpmf <mohammad.pfallah@gmail.com>
2026-02-16 10:56:09 +0330

    finish js-tut (season 2)

---
+++ js-tut/functions.js
@@ -0,0 +1,10 @@
+const dublicate = (number) => number * 2;
+const name = "Mohammad";
+const alaki = () => console.log("Helloooooo")
+
+// Named
+// تو روش نیمد میتونیم چندین تا رو اکسپورت کنیم.
+export { dublicate, name };
+
+// Default
+export default alaki
--- js-tut/script.js
+++ js-tut/script.js
@@ -51,30 +51,106 @@
 // const text = `My name is ${name} and my age is ${age} years old.`
 // console.log(text)
 // __________________ Destructuring ____________________
-const data = {
-    name: "Mohammad",
-    age: 35,
-    address: { city: "Rasht", street: "Azadegan" },
-}
-// const name = data.name
-// const age = data["age"]
-// const city = data.address.city
-// ...
-// yek raveshe sade tar (Destructuring)
-// const { name, address, age } = data
-// const { city, street } = address
-// in 2 marhale i bood. dakhelesh ham meshe dobare destructre kard ba : . yani in shekli:
-// const { name address: { street, city }, age } = data
-// montaha age khode address ro lazem darim, oonam joda minvisim yani in shekli:
-const {
-    name,
-    address,
-    address: { street, city },
-    age
-} = data
-// تو ریکت خیلی استفاده میشه.
+// const data = {
+//     name: "Mohammad",
+//     age: 35,
+//     address: { city: "Rasht", street: "Azadegan" },
+// }
+// // const name = data.name
+// // const age = data["age"]
+// // const city = data.address.city
+// // ...
+// // yek raveshe sade tar (Destructuring)
+// // const { name, address, age } = data
+// // const { city, street } = address
+// // in 2 marhale i bood. dakhelesh ham meshe dobare destructre kard ba : . yani in shekli:
+// // const { name address: { street, city }, age } = data
+// // montaha age khode address ro lazem darim, oonam joda minvisim yani in shekli:
+// const {
+//     name,
+//     address,
+//     address: { street, city },
+//     age
+// } = data
+// // تو ریکت خیلی استفاده میشه.
+// console.log(name)
+// console.log(age)
+// console.log(address)
+// console.log(city)
+// console.log(street)
+// __________________spread operator ____________________
+// const numbers1 = [1, 2, 3, 4]
+// const letters = ["a", "s", "d"]
+// const numbers2 = [6, ...numbers1, 8, 9, 0, ...letters]
+// console.log(numbers1);
+// console.log(numbers2);
+// _________async js - promise ____________________
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// _________async/await ____________________
+// async function getData() {
+//       const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       json = await res.json()
+//       console.log(json)
+// }
+// روش دوم و اگر به صورت ارو فانکشن باشه.
+// const getData = async () => {
+//       const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       json = await res.json()
+//       console.log(json)
+// }
+// تمرین اضافه خودم برای پارس کردن جیسون
+// async function getData() {
+//       const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       console.log(res)
+//       json = await res.json()
+//       console.log(json)
+//       const { completed, id, userId, title } = json
+//       console.log(userId)
+//       console.log(id)
+//       console.log(title)
+//       console.log(completed)
+// }
+// تمرین اضافه بیشتر
+// const getData = async () => {
+//       const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
+//       console.log(res)
+//       json = await res.json()
+//       console.log(json)
+//       const { name, id, username, email, address,
+//             address: { street, suite, city, zipcode, geo, geo: { lat, lng }, },
+//             phone, website, company,
+//             company: { name: companyName, catchPhrase, bs } // اینجا رو از دیپ سیک پرسیدم. چون کلمه نیم تکراری بود و ارور میداد. با : میتونیم بهش بگیم که یه اسم جدید براش بذاره.
+//       } = json
+//       console.log(id)
+//       console.log(name)
+//       console.log(username)
+//       console.log(email)
+//       console.log(address)
+//       console.log(street)
+//       console.log(suite)
+//       console.log(city)
+//       console.log(zipcode)
+//       console.log(geo)
+//       console.log(lat)
+//       console.log(lng)
+//       console.log(phone)
+//       console.log(website)
+//       console.log(company)
+//       console.log(companyName)
+//       console.log(catchPhrase)
+//       console.log(bs)
+// }
+// getData()
+// _________export/import ____________________
+// import alaki from "./functions.js";
+// import { dublicate, name } from "./functions.js";
+// یا ایمپورت یکجای نیمد و دیفالت با هم
+import alaki, { dublicate, name } from "./functions.js";
+console.log(dublicate(3))
 console.log(name)
-console.log(age)
-console.log(address)
-console.log(city)
-console.log(street)
+alaki()
