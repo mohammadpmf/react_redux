8c07d6df80a67acc9a83bab36e367540dcd7a2ba
mohammadpmf <mohammad.pfallah@gmail.com>
2026-02-28 16:21:52 +0330

    finish Season 9 (contact-app)

---
+++ contact-app/src/components/ContactItem.jsx
@@ -0,0 +1,17 @@
+import styles from "./ContactItem.module.css"
+
+function ContactItem({
+    data: { id, name, lastName, email, phone },
+    deleteHandler
+}) {
+    return (
+        <li className={styles.item}>
+            <p>{name} {lastName}</p>
+            <p><span>📩</span> {email} </p>
+            <p><span>☎</span> {phone} </p>
+            <button onClick={() => deleteHandler(id)}>🚮</button>
+        </li>
+    )
+}
+
+export default ContactItem
